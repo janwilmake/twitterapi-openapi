892b55f537ede467cdf7146d832205f7f81b5d3b
Jan Wilmake <jan@wilmake.com>
2025-09-15 10:48:16 +0200

    improvements

--- x-search-mcp/main.ts
+++ x-search-mcp/main.ts
@@ -6,11 +6,14 @@ export default {
   fetch: withMcp(
     (request) => {
       const url = new URL(request.url);
-      const apiKey =
-        request.headers.get("x-api-key") || url.searchParams.get("apiKey");
+      const headerApiKey = request.headers.get("x-api-key");
+      const queryApiKey = url.searchParams.get("apiKey");
+      const apiKey = headerApiKey || queryApiKey;
       if (!apiKey) {
         return new Response("No API key provided", { status: 400 });
       }
+      console.log({ headerApiKey, queryApiKey, apiKey });
+
       return fetch("https://api.twitterapi.io" + url.pathname + url.search, {
         method: request.method,
         body: request.body,
